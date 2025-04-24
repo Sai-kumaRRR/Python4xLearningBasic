c4bfbeedf4520cfe83fbe9cb3f0de695a54abf06
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-04-24 09:17:30 +0530

    24th April 2025

---
+++ 06.02.2025/python_loop.py
@@ -0,0 +1,11 @@
+# loop
+# Repeat a block of code multiple times
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+# print("hello world")
+
+for x in range(1, 10, 2):
+    print(x)
+    # range (start ,stop, step)
