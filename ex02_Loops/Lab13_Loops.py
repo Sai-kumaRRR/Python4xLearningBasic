651f4854d687060c8452223c4bb6d0b9f7a633a9
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-05-16 14:35:02 +0530

    16th May 2025

---
+++ ex02_Loops/Lab13_Loops.py
@@ -0,0 +1,12 @@
+
+ # While Loops
+
+name = input("Enter your name =")
+
+x = 0
+
+while x <= 10:
+
+    print(name)
+
+    x = x + 1   # 10 + 1
---
+++ ex02_Loops/Lab15_PrintTable.py
@@ -0,0 +1,25 @@
+""""""""
+2
+
+
+""""""
+
+number = int(input("enter number = "))
+number2 = int(input("enter number = "))
+number3 = int(input("enter number = "))
+
+x = 2
+y = 6
+z = 8
+
+while x <= 10:
+    print(f"{number} * {x} = {number * x}")
+    x = x + 2
+
+while y <= 20:
+    print(f"{number} * {y} = {number * y}")
+    y = y + 3
+
+while z <= 30:
+    print(f"{number} * {x} = {number * x}")
+    z = z + 6
